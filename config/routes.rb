c60eed16ab91243e108c070f54279576036e7085
Darcey Lachtman <dslachtman@gmail.com>
2014-05-06 13:05:41 -0700

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Portfolio::Application.routes.draw do
 
   root :to => 'pages#index'
-  
+  # resume :to => 'pages#resume'
+ 
+  # resources :pages, only: :show
+
+  get '/projects', to: 'pages#projects'
+  get '/vectors', to: 'pages#vectors'
+  get '/photography', to: 'pages#photography'
+  get '/resume', to: 'pages#resume'
+  get '/contact', to: 'pages#contact'
+  get '/about', to: 'pages#about'
+
 end
